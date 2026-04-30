34e1ce65c5bac0907797ee12cb7856f38e4e3a76
Ali Görkem Küçük <85097044+TrussedBoat@users.noreply.github.com>
2026-04-30 20:47:20 +0300

    added dataset link and code repo links

--- project-links.js
+++ project-links.js
@@ -1,7 +1,7 @@
 window.PROJECT_LINKS = {
   paper: "PAPER_PAGE_LINK_HERE",
   arxiv: "ARXIV_LINK_HERE",
-  code: "MAIN_CODE_REPO_LINK_HERE",
-  modelRepo: "https://github.com/GazeForHRI/GazeModels/tree/main",
-  dataset: "DATASET_LINK_HERE"
+  code: "https://github.com/GazeForHRI/Gaze4HRI",
+  modelRepo: "https://github.com/GazeForHRI/GazeModels",
+  dataset: "https://drive.google.com/drive/folders/1ubiHeMHoHYO4jhBJ71hVH4rFAYd0VefY?usp=sharing"
 };
