cfa9ef3f7537c7da775ffacadd8abf043e069bc0
berksezer9 <berksezer9@gmail.com>
2026-05-08 13:51:05 +0300

    add arxiv link

--- project-links.js
+++ project-links.js
@@ -1,6 +1,6 @@
 window.PROJECT_LINKS = {
   paper: "PAPER_PAGE_LINK_HERE",
-  arxiv: "ARXIV_LINK_HERE",
+  arxiv: "https://arxiv.org/abs/2605.04770",
   code: "https://github.com/GazeForHRI/Gaze4HRI",
   modelRepo: "https://github.com/GazeForHRI/GazeModels",
   dataset: "https://drive.google.com/drive/folders/1ubiHeMHoHYO4jhBJ71hVH4rFAYd0VefY?usp=sharing"
