8b167c124079699f0d76ac873cfbd6c2d2eb84dc
berksezer9 <110352157+berksezer9@users.noreply.github.com>
2026-06-20 21:57:15 +0300

    Update paper link in project-links.js

--- project-links.js
+++ project-links.js
@@ -1,5 +1,5 @@
 window.PROJECT_LINKS = {
-  paper: "PAPER_PAGE_LINK_HERE",
+  paper: "https://arxiv.org/abs/2605.04770",
   arxiv: "https://arxiv.org/abs/2605.04770",
   code: "https://github.com/GazeForHRI/Gaze4HRI",
   modelRepo: "https://github.com/GazeForHRI/GazeModels",
